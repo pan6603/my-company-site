b4dea57bbe24ce0be86e59461c3392edfd3ee8af
pan6603 <pan5158a@gmail.com>
2026-02-22 15:16:00 +0900

    CTA Section UI 구현

--- components/sections/CTA.tsx
+++ components/sections/CTA.tsx
@@ -5,8 +5,26 @@
 export default function CTA() {
     return (
         <>
-            <div className="w-full max-w-[1216px] h-[346px] bg-yellow-200 mt-[65px] m-auto">
-                Call to Action Section
+            <div className="w-full max-w-[1216px] h-[346px] bg-[#137FEC] rounded-3xl mt-[65px] m-auto flex justify-center items-center">
+                <div className="w-full max-w-[672px] h-[218px] flex flex-col justify-between items-center">
+                    <div className="w-full max-w-[672px] h-[40px] text-[#FFFFFF] text-4xl flex justify-center items-center">
+                        Ready to transform your workflow?
+                    </div>
+
+                    <div className="w-full max-w-[672px] h-[56px] text-center text-[#FFFFFF] text-lg">
+                        Join thousands of teams who trust SaaSFlow to power their business. Start <br />
+                        your 14-day free trial today.
+                    </div>
+
+                    <div className="w-full max-w-[672px] h-[74px] flex justify-center items-center gap-2">
+                        <button className="w-full max-w-[180px] h-[58px] bg-[#FFFFFF] text-[##137FEC] font-bold text-base rounded-lg cursor-pointer">
+                            Start Free Trial
+                        </button>
+                        <button className="w-full max-w-[176px] h-[58px] bg-[#137FEC] text-[#FFFFFF] border border-white font-bold text-base rounded-lg cursor-pointer">
+                            Contact Sales
+                        </button>
+                    </div>
+                </div>
             </div>
         </>
     )
