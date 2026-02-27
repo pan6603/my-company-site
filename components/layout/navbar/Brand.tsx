39341b1b5d4e1bc669c3e8636627eb28e984a956
pan6603 <pan5158a@gmail.com>
2026-02-27 15:39:15 +0900

    메인 이동 링크 추가

--- components/layout/navbar/Brand.tsx
+++ components/layout/navbar/Brand.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
 import logoIcon from "../../../public/images/logo.svg"
-
+import Link from "next/link"
 
 export default function Brand() {
     return (
         <>
-            <div className="w-full max-w-[133px] h-[32px] flex justify-between items-center">
+            <Link href="/" className="w-full max-w-[133px] h-[32px] flex justify-between items-center">
                 <div className="w-full max-w-[32px] h-[32px cursor-pointer">
                     <Image 
                         src={logoIcon}
@@ -16,7 +16,7 @@ export default function Brand() {
                 </div>
 
                 <span className="text-[#0F172A] text-xl font-bold cursor-pointer">SaaSFlow</span>
-            </div>
+            </Link>
         </>
     )
 }
