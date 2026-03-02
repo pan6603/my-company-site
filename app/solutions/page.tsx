cecd03ab03da46817e1f61fbba3e0a06d7dbf226
pan6603 <pan5158a@gmail.com>
2026-03-02 16:19:05 +0900

    SolutionsPage UI 수정

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -5,6 +5,7 @@ import SolutionsHereSection from "@/components/sections/SolutionsHereSection"
 export default function SolutionsPage() {
     return (
         <>
+            <div className="w-full max-w-[1440px] h-[3098px] bg-gray-100 flex flex-col items-center"></div>
             <SolutionsHereSection />
         </>
     )
